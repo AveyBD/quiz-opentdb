18b26f679e0d6def837b729d7c9430ef0e84b8ea
Debashish D. Dev <hello@debashish.dev>
2022-07-06 00:25:40 +0600

    Solved Duplicate even after Suffle

--- src/Components/Quiz/Question.jsx
+++ src/Components/Quiz/Question.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 
 const Question = ({ item, index }) => {
-  const changeCorrectAnsLocation = (answers) => {
+  const changeCorrectAnsLocation = (answers, main) => {
     for (let m = answers.length - 1; m > 0; m--) {
       const n = Math.floor(Math.random() * (m + 1));
       [answers[m], answers[n]] = [answers[n], answers[m]];
+    }
+    if (main === answers) {
+      changeCorrectAnsLocation(answers);
+    } else {
       return answers;
     }
   };
-  // console.log(item);
+
   const { question, correct_answer, incorrect_answers } = item;
   const options = [...incorrect_answers, correct_answer];
+  const main = [...incorrect_answers, correct_answer];
   console.log(`Line 14: ${options}`);
-  changeCorrectAnsLocation(options);
+  changeCorrectAnsLocation(options, main);
   console.log(`Line 16: ${options}`);
   return (
     <div>
